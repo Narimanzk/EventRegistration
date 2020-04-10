ada7da83798442a6d594bb515d23ffae130f0e3c
Nariman <nariman.zendehrooh@gmail.com>
2020-04-09 23:13:50 -0400

    Update index.js

--- EventRegistration-Frontend/config/index.js
+++ EventRegistration-Frontend/config/index.js
@@ -4,9 +4,9 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    host: 'eventregistration-frontend-asg.herokuapp.com',
+    host: 'eventregistration-frontend-123.herokuapp.com',
     port: 443,
-    backendHost: 'eventregistration-backend-asg.herokuapp.com',
+    backendHost: 'eventregistration-backend-123.herokuapp.com',
     backendPort: 443,
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
